3c5c4e4cb14931b899dbdce8b08200818a47bc92
TevinThuku <tevinthuku@gmail.com>
2019-01-31 22:29:04 +0300

    [Finishes #163549189] Can View parties list that has been mocked via JS

--- UI/Js/validation.js
+++ UI/Js/validation.js
@@ -11,3 +11,25 @@ function doesFieldContainValidValue(value) {
   return /\S/.test(value) && Boolean(value);
 }
 
+function formatDate(date) {
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + " " + monthNames[monthIndex] + " " + year;
+}
