557ed28014ba41bbad43109a8cc67f965152b57a
TevinThuku <tevinthuku@gmail.com>
2019-02-06 00:09:14 +0300

    [Finishes #163674628] Edit party name

--- app/api/v1/model/__init__.py
+++ app/api/v1/model/__init__.py
@@ -31,10 +31,29 @@ class PartiesModel:
     def save_party(self):
         PARTIES.append(self)
 
+    def setname(self, newname):
+        """
+            sets the name of the object to a new name provided in the parameter
+        """
+        self.name = newname
+
     @staticmethod
     def get_all_parties():
+        """
+            returns a list of all objects with only their attributes
+        """
         return [vars(party) for party in PARTIES]
 
     @staticmethod
+    def get_party_object(id):
+        """
+            returns the object with all attributes and functions as well
+        """
+        return [party for party in PARTIES if party.id == id]
+
+    @staticmethod
     def get_party(id):
+        """
+            returns the object with only the attributes. 
+        """
         return [vars(party) for party in PARTIES if party.id == id]
