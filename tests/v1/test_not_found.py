d86d389d13766d5f95e269e36268fbad520a02dd
TevinThuku <tevinthuku@gmail.com>
2019-02-07 13:30:13 +0300

    [Finishes #163771215] handled routes not defined in the app

---
+++ tests/v1/test_not_found.py
@@ -0,0 +1,24 @@
+from app import app
+import unittest
+import json
+
+
+class RoutesBaseTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app("testing")
+        self.client = self.app.test_client()
+    # tear down tests
+
+    def tearDown(self):
+        """Tperform final cleanup after tests run"""
+        self.app.testing = False
+
+
+class TestOfficesEndPoint(RoutesBaseTest):
+
+    def test_invalid_route(self):
+        response = self.client.get("api/vNotFound")
+        self.assertEqual(response.status_code, 404)
+        result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(result["error"], "url not found")
+        self.assertEqual(result["status"], 404)
