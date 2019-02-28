15ccdaed8a639696ebee569b08d7235e1846c7f6
Tevinthuku <tevinthuku@gmail.com>
2019-02-28 20:21:48 +0300

    Feat(v2): User Voting Activity endpoint
    - Create tests for the endpoint
    - Create the endpoint itself
    - Update README with new endpoints
    
    [Finishes #164306902]

--- app/api/v2/models/votes.py
+++ app/api/v2/models/votes.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from .offices import OfficesModel
+from .candidates import CandidateModel
+from .users import UserModel
 
 
 class VotesModel:
@@ -33,8 +36,36 @@ class VotesModel:
             post or not.
         """
         check_vote_query = """
-        SELECT voter, office FROM votes
+        SELECT voter, office, candidate FROM votes
         WHERE votes.voter = '{}' AND votes.office = '{}'
         """.format(voter, office)
         voted = db.select_data_from_db(check_vote_query)
         return voted
+
+    @staticmethod
+    def resolve_user_voting_activity(user_id):
+        """
+        This function returns the voting activity undertaken by a user.
+        for all the offices that are existent in the app.
+        """
+        data = []
+        for office in OfficesModel.get_all_offices():
+            info = None
+            voteinfo = VotesModel.check_if_user_already_voted(
+                user_id, office["id"])
+            if(voteinfo):
+                candidatevotedfor = UserModel.get_user_by_id(voteinfo[0][2])
+                # get candidate by id
+                info = "You have already voted for {} here".format(
+                    candidatevotedfor[0][1])
+            else:
+                info = OfficesModel.get_all_candidates(office["id"])
+            details = {
+                "id": office["id"],
+                "name": office["name"],
+                "type": office["type"],
+                "info": info
+            }
+
+            data.append(details)
+        return data
