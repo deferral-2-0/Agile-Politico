16fe2f2fc266a735fdbd3b19bb426e3d7b9198f4
Tevinthuku <tevinthuku@gmail.com>
2019-02-28 19:12:50 +0300

    Feat(UI): View list of contestants
    - Create function to fetch all contestants
    - Render contestants list on the DOM
    
    [Finishes #164307935]

---
+++ UI/Js/pages/contestants.js
@@ -0,0 +1,73 @@
+const groupBy = (items, key) =>
+  items.reduce(
+    (result, item) => ({
+      ...result,
+      [item[key]]: [...(result[item[key]] || []), item]
+    }),
+    {}
+  );
+
+fetch("https://tevpolitico.herokuapp.com/api/v2/offices/metainfo")
+  .then(data => data.json())
+  .then(data => groupBy(data.data, "type"))
+  .then(data => {
+    console.log(data);
+    let officetypelist = Object.keys(data);
+    let vdom = {
+      type: "div",
+      props: {},
+      children: officetypelist.map(type => ({
+        type: "div",
+        props: {},
+        children: [
+          {
+            type: "h1",
+            props: { class: "positiontitle" },
+            children: [`Office-type: ${type}`]
+          },
+          {
+            type: "div",
+            props: {},
+            children: data[type].map(office => ({
+              type: "div",
+              props: {},
+              children: [
+                {
+                  type: "h3",
+                  props: { class: "officename" },
+                  children: [office.name]
+                },
+                {
+                  type: "div",
+                  props: {},
+                  children:
+                    office.candidates.length > 0
+                      ? office.candidates.map(candidate => ({
+                          type: "h3",
+                          props: { class: "candidatesinfo" },
+                          children: [
+                            `${candidate.username} - Email: ${candidate.email}`
+                          ]
+                        }))
+                      : [
+                          {
+                            type: "h3",
+                            props: { class: "candidatesinfo" },
+                            children: [
+                              "No One is vying in this post contact the admin if you are interested"
+                            ]
+                          }
+                        ]
+                }
+              ]
+            }))
+          }
+        ]
+      }))
+    };
+    return vdom;
+  })
+  .then(vdom => {
+    const $contestants = document.getElementById("render-posts-&-contestants");
+    $contestants.appendChild(createElement(vdom));
+  });
