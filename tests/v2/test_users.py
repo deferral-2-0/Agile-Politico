36fbf8f39db71eea810c757a60553e0af801c385
bedann <bedank6@gmail.com>
2019-03-21 19:59:35 +0300

    164789447-story(users): get single user profile
    - add endpoint for getting single user data
    - update test_users.py with new endpoint test
    
    [Delivers #164789447]

--- tests/v2/test_users.py
+++ tests/v2/test_users.py
@@ -237,7 +237,9 @@ class TestUserEndpoints(BaseTestClass):
         res = self.client.get("api/v2/users")
         result = json.loads(res.data.decode("utf-8"))
         self.assertEqual(
-            len(result["data"]), 1)
+            result["data"], [
+                {'email': 'admindetails@gmail.com', 'id': 1,
+                    'username': 'OriginalAdmin', 'isAdmin': True}])
 
     def test_password_without_number(self):
         res = self.client.post("api/v2/auth/signup",
